df328db87ac3a7336e80f70bfc3734709d94c803
Monisha Raja <monisharaja086@gmail.com>
2026-08-28 22:55:13 +0530

    Fix CORS configuration

--- backend/main.py
+++ backend/main.py
@@ -44,11 +44,13 @@ app.include_router(github_router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
+    "http://localhost:5175",
+    "http://127.0.0.1:5175",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
